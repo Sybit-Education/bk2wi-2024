8c2d27ca1112bc6d4098387731289e46d1b55ab1
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-10-30 14:58:32 +0000

    chore: Footer enhanced

--- src/components/navigation/FooterBar.vue
+++ src/components/navigation/FooterBar.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="footer-bar">&copy; 2024 - BK2WI 2024 - BSZ Radolfzell</div>
+  <div class="footer-bar">
+    &copy; 2024 - BK2WI 2024 - BSZ Radolfzell in Kooperation mit
+    <a href="https://sybit.education">Sybit GmbH</a>.
+  </div>
 </template>
 <style lang="css" scoped>
 .footer-bar {
