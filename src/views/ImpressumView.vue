726a41b574e08098ad162cb1c0c09700c466d386
ericnull77 <ericgobel645@gmail.com>
2025-01-27 13:34:45 +0000

    impressum aktualiesiert

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="impressum container mt-4">
     <h1>Impressum</h1>
-    <p>Angaben gemäß § 5 TMG</p>
-    <h2>Kontakt</h2>
-    <p>
+      <p>Angaben gemäß § 5 TMG</p>
+        <h2>Herausgeber</h2>
+        <p>
+
       Sportsy GmbH<br>
-      Musterstraße 123<br>
-      12345 Musterhausen
-    </p>
-    <h2>Vertreten durch</h2>
-    <p>Max Mustermann</p>
+      
+  
+       vertreten durch das Berufsschulzentrum Radolfzell<br>
+(Gewerbliche, Kaufmännische und Hauswirtschaftliche Schulen Radolfzell)<br>
+
+Alemannenstraße 15<br>
+78315 Radolfzell<br>
+Tel: 07732 989 113 / 07732 989 123 / 07732 989 133<br>
+Fax: 07732 989 220<br>
+E-Mail: info@bsz-radolfzell.de<br>
+
+<h3>Vertretungsberechtigte: </h3> 
+Leon, Lewin, Timo (Projektleitung)<br>
+E-Mail: Sportsy@verwaltung.bsz-radolfzell.de 
+<br><br>
+
+<h4> Haftung für Inhalte:</h4>
+Die Informationen, die Sie auf diesem Internetauftritt vorfinden, wurden nach bestem Wissen und Gewissen sorgfältig zusammengestellt und geprüft. 
+Für die Richtigkeit, Vollständigkeit, Aktualität oder Qualität und jederzeitige Verfügbarkeit der bereit ge-stellten Informationen wird jedoch keine Gewähr übernommen.
+Diensteanbieter sind gemäß § 7 Abs. 1 DDG für eigene Inhalte auf diesen Seiten verantwortlich.
+ Die Artikel 4 bis 8 der Verordnung (EU) 2022/2065 gelten für Dienste-anbieter einschließlich der öffentlichen Stellen, 
+ unabhängig davon, ob für die Nut-zung ein Entgelt erhoben wird. Diensteanbieter, die Nutzern einen Internetzugang über ein drahtloses lokales Netzwerk zur Verfügung stellen, können auf freiwilliger Basis die Nutzer identifizieren, eine Passworteingabe verlangen oder andere freiwil-lige Maßnahmen ergreifen. Sie dürfen hierzu jedoch nicht von einer Behörde ver-pflichtet werden (vgl. § 7 Abs. 2 DDG)
+Dieser Internetauftritt enthält auch Links oder Verweise auf Internetauftritte Dritter.
+ Diese Links zu den Internetauftritten Dritter stellen keine Zustimmung zu deren Inhal-ten durch den Herausgeber dar
+Für die Inhalte der externen Links sind die jeweiligen Anbieter oder Betreiber (Urhe-ber) der Seiten verantwortlich.
+ Mit den Links zu anderen Internetauftritten wird den Nutzern lediglich der Zugang zur Nutzung der Inhalte vermittelt.
+Die externen Links wurden zum Zeitpunkt der Linksetzung nach bestem Wissen und Gewissen auf eventuelle Rechtsverstöße überprüft. 
+Eine ständige inhaltliche Über-prüfung der externen Links ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht möglich.
+ Bei Verlinkungen auf die Webseiten Dritter, die außerhalb des Verant-wortungsbereichs des Herausgebers liegen, würde eine Haftungsverpflichtung nur bestehen,
+ wenn der Herausgeber von den rechtswidrigen Inhalten Kenntnis erlangen und es technisch möglich und zumutbar wäre, die Nutzung dieser Inhalte zu verhin-dern. 
+ Sollten Sie der Ansicht sein, dass die verlinkten externen Seiten gegen gelten-des Recht verstoßen oder sonst unangemessene Inhalte haben, so teilen Sie uns dies bitte mit.
+       </p>
+
   </div>
 </template>
 
@@ -20,5 +49,9 @@
 <style scoped>
 .impressum {
   max-width: 800px;
+
 }
+ 
+
+
 </style>
